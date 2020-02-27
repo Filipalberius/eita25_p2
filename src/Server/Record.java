2ef381d43b1eaa7c85c55d54792eaea439c06264
el0860de-s <elmer@dellson.com>
2020-02-27 13:26:47 +0100

    Changed Journal to Record

---
+++ src/Server/Record.java
@@ -0,0 +1,13 @@
+package Server;
+
+import java.io.File;
+
+public class Record {
+    private String patient, doctor, nurse, division;
+    private File record; //name.txt
+
+    public Record(File record) {
+
+
+    }
+}
